8f2e20424432ed7cc5884576ccfaa3b53e2b4486
Ethan Toney <ethan.toney@uky.edu>
2015-09-30 12:10:26 -0400

    Fixes for Windows

--- run.sh
+++ run.sh
@@ -4,14 +4,26 @@
 OIFS=$IFS
 IFS=$(echo -en "\n\b")
 
-FILES="$2*.in"
+FILES="$4*.in"
 
 TIMEOUT=3
+
+case $1 in
+	java)
+		COMMAND="java -cp '$2.$3'"
+	;;
+	c++11)
+		COMMAND="$2/$3"
+	;;
+esac
+
+echo $($COMMAND)
 
 for f in $FILES
 do
 	echo "----- starting $f -----"
-	echo "$(timeout -s 3 $TIMEOUT $1 < $f)"
+	# echo "$(timeout -s 3 $TIMEOUT $COMMAND < $f)"
+	echo "$($COMMAND < $f)"
 	echo "----- finished -----"
 done
 
